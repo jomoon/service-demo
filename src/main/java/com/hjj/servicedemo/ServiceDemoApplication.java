15edfcd39ae727aeda51f9adbfd96cc68caeb3de
jomoon <jo_moon@163.com>
2018-10-30 00:49:13 +0800

    init

---
+++ src/main/java/com/hjj/servicedemo/ServiceDemoApplication.java
@@ -0,0 +1,14 @@
+package com.hjj.servicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDemoApplication.class, args);
+    }
+}
